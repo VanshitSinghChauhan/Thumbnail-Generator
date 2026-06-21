fd73dab71bb7d83e3834e282377b59176462b0cf
VanshitSinghChauhan <vanshitsinghchauhan16032005@gmail.com>
2026-06-21 10:45:03 +0530

    Create empty state for thumbnails

--- frontend/react_js/reactjs/src/pages/MyGeneration.tsx
+++ frontend/react_js/reactjs/src/pages/MyGeneration.tsx
@@ -1,15 +1,15 @@
-import type { IThumbnail } from "../assets/assets";
+import { dummyThumbnails, type IThumbnail } from "../assets/assets";
 import { useState, useEffect } from "react";
 import SoftBackdrop from "../components/SoftBackdrop";
 
 export const MyGeneration = () => {
 
-  const [thumbnails, setThumbnails] = useState<IThumbnail>([])
-  const [loading, setLoading] = useState(true)
+  const [thumbnails, setThumbnails] = useState<IThumbnail[]>([])
+  const [loading, setLoading] = useState(false)
 
   const fetchThumbnails = async()=>{
     setThumbnails(dummyThumbnails as unknown as IThumbnail[])
-    //setLoading(false)
+    setLoading(false)
   }
   const handleDownlaod = (image_url: string) => {
     window.open(image_url, '_blank')
@@ -30,17 +30,26 @@ export const MyGeneration = () => {
       <SoftBackdrop />
       <div className="mt-32 min-h-screen px-6 md:px-16 lg:px-24 xl:px-32">
 
+        {/*Header */}
         <div className="mb-8 ">
           <h1 className="text-2xl font-bold text-zinc-200">My Collections</h1>
           <p className="text-small text-zinc-400 mt-1">View and manage all your AI-Generated thumbnails</p>
         </div>
 
-
+        {/*Loading */}
         {loading && (
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {Array.from({length : 6}).map((_, i)=>(
               <div className="rounded-2xl bg-white/6 border border-white/10 animate-pulse h-[260px]" key={i}></div>
             ))}
+          </div>
+        )}
+
+        {/*Empty State*/}
+        {!loading && thumbnails.length === 0 && (
+          <div className="text-center py-24">
+            <h3 className="text-lg font-semibold text-zinc-200">No Thumbnails Yet</h3>
+            <p text-sm text-zinc-400 mt-2>Generate your first thumbnail to see it here</p>
           </div>
         )}
 
