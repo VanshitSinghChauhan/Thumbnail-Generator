fa63aadbf82a8edadcedaaab6778f6f8c723c31a
VanshitSinghChauhan <vanshitsinghchauhan16032005@gmail.com>
2026-06-30 22:13:21 +0530

    Create Signup AuthContext

--- frontend/react_js/reactjs/src/context/AuthContext.tsx
+++ frontend/react_js/reactjs/src/context/AuthContext.tsx
@@ -1,5 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import type { IUser } from "../assets/assets";
+import api from "../configs/api";
+import toast from "react-hot-toast";
 
 interface AuthContextProps{
     isLoggedIn: boolean;
@@ -26,8 +28,19 @@ export const AuthProvider = ({children}:{children: React.ReactNode}) => {
     const [user, setUser] = useState<IUser | null>(null)
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false) 
 
-    const signUp = async ()=>{}
-    const login = async ()=>{}
+    const signUp = async ({name, email, password} : {name: string; email: string; password: string})=>{
+        try {
+            const {data} = await api.post('/api/auth/register', {name, email, password});
+            if(data.user){
+                setUser(data.user as IUser)
+                setIsLoggedIn(true)
+            }
+            toast.success(data.message)
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    const login = async (   )=>{}
     const logout = async ()=>{}
     const fetchUser = async ()=>{}
 
